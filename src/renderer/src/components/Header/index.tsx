e31e6f76321938eb6248a8e8ec1a8bdc9c6fc3ff
Ahmad Ahandani <a.e.ahandani@gmail.com>
2024-12-30 18:24:16 -0800

    chore: add ubuntu

--- src/renderer/src/components/Header/index.tsx
+++ src/renderer/src/components/Header/index.tsx
@@ -1,10 +1,10 @@
-import { Box, Heading, Icon } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import { GoTools } from 'react-icons/go'
 
 export const Header = () => (
   <Box
     css={{
-      '-webkit-app-region': 'drag'
+      WebkitAppRegion: 'drag'
     }}
     display={'flex'}
     justifyContent={'flex-end'}
@@ -15,8 +15,8 @@ export const Header = () => (
     <Heading lineHeight="tall" size={'xs'} fontWeight={'bold'}>
       TEXTWRENCH
     </Heading>
-    <Icon fontSize="xl" ml={2}>
-      <GoTools />
-    </Icon>
+    <Box ml={2}>
+      <GoTools fontSize="xl" />
+    </Box>
   </Box>
 )
