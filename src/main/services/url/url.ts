6fcf69c8df2170dd710f08f3bc3bbc7fa28cc9c9
Ahmad Ahandani <a.e.ahandani@gmail.com>
2025-02-28 23:28:41 -0800

    fix: fix invalid url error for windows

--- src/main/services/url/url.ts
+++ src/main/services/url/url.ts
@@ -1,3 +1,4 @@
+import log from 'electron-log'
 import { IPC_EVENTS } from '../../../shared/ipc-events'
 import { updateStore } from '../../store/helpers'
 import { getMainWindow } from '../window/window'
@@ -5,13 +6,17 @@ import { getMainWindow } from '../window/window'
 export function handleOpenUrl(url): void {
   const mainWindow = getMainWindow()
 
-  const urlParams = new URL(url)
-  const token = urlParams.searchParams.get('token')
+  try {
+    const urlParams = new URL(url)
+    const token = urlParams.searchParams.get('token')
 
-  if (token) {
-    updateStore('token', token)
-    mainWindow?.webContents.send(IPC_EVENTS.LOGIN_FULFILLED, { token })
-  } else {
-    mainWindow?.webContents.send(IPC_EVENTS.LOGIN_FULFILLED)
+    if (token) {
+      updateStore('token', token)
+      mainWindow?.webContents.send(IPC_EVENTS.LOGIN_FULFILLED, { token })
+    } else {
+      mainWindow?.webContents.send(IPC_EVENTS.LOGIN_FULFILLED)
+    }
+  } catch (error) {
+    log.error('Invalid URL:', url, error)
   }
 }
